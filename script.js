adad4c0b7c984bd041117eab0fc5a77edca67170
RaulDM <raul.delgado@hotmail.com>
2025-07-29 16:12:45 +0200

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ejemplo de JavaScript para un scroll suave en la navegación
+document.querySelectorAll('nav ul li a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
