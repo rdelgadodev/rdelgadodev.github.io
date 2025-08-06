fd015e0d6e8154c558758d82d3b5be5ea833f81f
RaulDM <raul.delgado@hotmail.com>
2025-08-06 07:44:06 +0200

    responsive

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// // Ejemplo de JavaScript para un scroll suave en la navegación
-// document.querySelectorAll('nav ul li a[href^="#"]').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//         e.preventDefault();
-
-//         document.querySelector(this.getAttribute('href')).scrollIntoView({
-//             behavior: 'smooth'
-//         });
-//     });
-// });
-
 // Hacer aparecer el menu
 // Declaramos dos constante que serán nuestro boton, el menu y los span
 const btn_menu = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 const btn_span = document.querySelectorAll("span");
 
+// Función para hacer el efecto de convertir el menu hamburguesa en una X de cerrar.
 function menu_toggle() {
-    // Almacenamos 
+    // Almacenamos en una variable el valor del data del botón del menu
     const is_open = btn_menu.dataset.menuOpen;
+      
     if (is_open === "false") {
         menu.classList.add("is_visible");
         btn_menu.dataset.menuOpen = "true";
@@ -34,3 +25,17 @@ function menu_toggle() {
 }
 
 btn_menu.onclick = menu_toggle;
+
+// Constante con los items del menu, para que se cierre el menu cada vez que se haga clic sobre uno
+const items = document.querySelectorAll('.menu li')
+
+// La función quitará la clase 'is_visible' para que no se muestre por pantalla y llamará a la función de volver a mostrar el menu hamburguesa
+function close_menu(){
+      menu.classList.remove("is_visible");
+      menu_toggle()
+}
+
+// Se recorre cada item para que independientemente de en cual se haga clic, se cierre el menú.
+items.forEach(item => {
+      item.onclick = close_menu
+});
